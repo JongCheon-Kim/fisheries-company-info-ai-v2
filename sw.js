662585b095a0688b7a70a95474afc211be4cb743
JongCheon-Kim <123dipika@naver.com>
2026-08-18 08:32:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="kafv-fish-company-info-ai-v2-1-20260817";
+const CACHE_NAME="kafv-fish-company-info-ai-v2-2-20260818";
 const SHELL=["./","./index.html","./manifest.json","./kafv-fish-company-ai-icon-192.png","./kafv-fish-company-ai-icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
